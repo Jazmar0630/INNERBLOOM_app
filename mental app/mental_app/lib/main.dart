c298bd147c060dd8109818e2158c4d4bf1f009c4
Jazmar0630 <ammarasri15@gmail.com>
2025-11-11 21:28:22 +0800

    edit

--- mental app/mental_app/lib/main.dart
+++ mental app/mental_app/lib/main.dart
@@ -6,7 +6,7 @@ import 'pages/mood_tracker_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
+  
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
