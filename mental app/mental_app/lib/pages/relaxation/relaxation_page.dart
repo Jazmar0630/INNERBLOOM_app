14398be4af1079c31be4087557c493ee8fd9c527
ahmdsyawqi <ahmdsywqi@gmail.com>
2025-12-01 10:53:02 +0800

    update

--- mental app/mental_app/lib/pages/relaxation/relaxation_page.dart
+++ mental app/mental_app/lib/pages/relaxation/relaxation_page.dart
@@ -5,7 +5,7 @@ import '../home/home_page.dart';
 import '../mood/onboarding_intro_page.dart';
 import '../user/user_page.dart';
 =======
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import '../home/home_page.dart';
 import '../mood/onboarding_intro_page.dart';
 import '../user/user_page.dart';
