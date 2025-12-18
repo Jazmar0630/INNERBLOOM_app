b077a5df8013eb93115364fd145302d23386ef39
Jazmar0630 <ammarasri15@gmail.com>
2025-12-18 19:13:13 +0800

    relaxation service page

---
+++ lib/services/relaxation_service.dart
@@ -0,0 +1,30 @@
+ import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../config/api_config.dart';
+
+class RelaxationService {
+  static Future<String> generateRelaxation({
+    required String mood,
+  }) async {
+    final uri = Uri.parse('${ApiConfig.baseUrl}/generateRelaxation');
+
+    final response = await http.post(
+      uri,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'mood': mood,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['result']; // adjust to your backend response
+    } else {
+      throw Exception(
+        'Failed to generate relaxation (${response.statusCode})',
+      );
+    }
+  }
+}
